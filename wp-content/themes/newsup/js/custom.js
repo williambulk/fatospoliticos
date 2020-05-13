39c5e28bcbf7e743c55e7ce68d180aed56c2dbda
William Kunitake <william.kunitake@gmail.com>
2020-05-13 00:46:45 -0400

    Remoção do timer de segundos

--- wp-content/themes/newsup/js/custom.js
+++ wp-content/themes/newsup/js/custom.js
@@ -140,15 +140,3 @@ function colmnthree() {
   });
 }
 colmnthree();
-
-jQuery(document).ready(function($) {
- /*Time*/
-  var myVar = setInterval(function() {
-    myTimer();
-  }, 100);
-
-  function myTimer() {
-    var d = new Date();
-    document.getElementById("time").innerHTML = d.toLocaleTimeString();
-  }
-});
